1805a43d2654625be57a5486f5da9c999e65db38
yungyeong <rachel@brich.co.kr>
2022-11-08 12:30:56 +0900

    [feature] Calculate 구문 완성(+주석)

--- src/main/java/level1/calculate/Calculate.java
+++ src/main/java/level1/calculate/Calculate.java
@@ -3,6 +3,14 @@ package level1.calculate;
 public class Calculate {
 
     public static int sum(int a, int b) {
-        return 0;
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
     }
 }
--- src/test/java/level1/calculate/CalculateTest.java
+++ src/test/java/level1/calculate/CalculateTest.java
@@ -1,6 +1,8 @@
 package level1.calculate;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,11 +13,20 @@ public class CalculateTest {
     @Test
     @DisplayName("1+2 의 결과는 3")
     void sum() {
+        // given
         int a = 1;
         int b = 2;
 
-        int result = a + b;
 
+        // when
+//        int result = a + b;
+        int result = Calculate.sum(a,b);
+
+
+        // then
+        // assert -> 검증한다
+        // 테스트코드 작성할 때는 실패한 코드를 먼저 작성한다.
+        // 프로덕션 코드를 짜기 전에 실패 -> 하드코딩 -> 리팩토링 과정을 거치는 것이 디버깅하기에도 좋다.
         assertEquals(3, result);
     }
 
@@ -25,6 +36,9 @@ public class CalculateTest {
         int a = 4;
         int b = 2;
 
+        int result = Calculate.minus(a,b);
+
+        assertEquals(2, result);
     }
 
 
@@ -34,6 +48,12 @@ public class CalculateTest {
         int dividend = 4;
         int divisor = 2;
 
+        int result = Calculate.divide(dividend, divisor);
+
+        boolean bool = true;
+        assertTrue(bool);
+//        assertEquals(2, result);
+//        assertThat((result).isEqualTo(2));
     }
 
 }
